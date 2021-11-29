aa1798c37529ded133b2218b58c96845afbd1b3c
ChidoGang <edgar_quintanilla@mymail.rcbc.edu>
2021-11-29 13:07:03 -0500

    created css set up handle bars template

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,9 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
 const tutorRoutes = require('./tutor-routes');
+const homeRoutes = require('./home-routes.js');
 
+router.use('/', homeRoutes);
 router.use('/users', userRoutes);
 router.use('/tutors', tutorRoutes);
 
