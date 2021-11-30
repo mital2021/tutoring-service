a4e15404a6f7470d7bbd69d6daa0593672f064f5
ChidoGang <edgar_quintanilla@mymail.rcbc.edu>
2021-11-29 20:02:50 -0500

    edited hbs added home routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const tutorRoutes = require('./tutor-routes');
 const homeRoutes = require('./home-routes.js');
+const controllers = require('./controllers/'); 
 
 router.use('/', homeRoutes);
 router.use('/users', userRoutes);
